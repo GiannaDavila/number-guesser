665cf7cff1eb558c7408c0fcfd58bcbde7ba39f0
GiannaDavila <giannagiannadavila@gmail.com>
2022-10-13 09:35:30 -0400

    first commit

---
+++ number-guesser.js
@@ -0,0 +1,9 @@
+/* 
+create a program that guesses a number from 1 to 10 
+then ask us to ask us to guess that number.
+if its higher say "too high" and ask again.
+if its loweer say "too low" and ask again.
+if its the right number say "congradulations" and exit
+*/
+
+import readline from "readline-sync"
