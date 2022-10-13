2d799f7bb37e439d10343a097dd0dd5ae4676bab
GiannaDavila <giannagiannadavila@gmail.com>
2022-10-13 10:46:00 -0400

    first commit

--- number-guesser.js
+++ number-guesser.js
@@ -7,3 +7,33 @@ if its the right number say "congradulations" and exit
 */
 
 import readline from "readline-sync"
+
+let userGuessedRight = false
+
+// while (true){
+// guess a number from 1-10
+let NumberChosen = Math.ceil(Math.random() * 10)
+// console.log(NumberChosen)
+while (!userGuessedRight) {
+    let guess = readline.question("what is your guess?")
+    // console.log(guess)
+    if (guess == NumberChosen) {
+        console.log('congradulations')
+        userGuessedRight = true
+    }
+    else if (guess > NumberChosen) {
+        console.log('too high. try again!')
+
+    }
+    else if (guess < NumberChosen) {
+        console.log('too low!')
+
+    }
+    else if (typeof guess != "number") {
+        console.log('Not a Number')
+    }
+}
+// while(!userGUessedRight){
+// console.log("What is your guess?")
+
+// }
